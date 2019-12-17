86d961ae3a8cd4ce7281ef12a55a3ba54be66ae7
Toni Lamont <tlam212@gmail.com>
2019-12-16 21:44:36 -0500

    added controller and routes

---
+++ todo-api/app/controllers/api/v1/todo_controller.rb
@@ -0,0 +1,52 @@
+class Api::V1::TodoController < ApplicationController
+
+  def index
+    @todos = Todo.all
+    render json: @todos, status: 200
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todo, status: 200
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    rrender json: @todo, status: 200
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    render json: @todo, status: 200
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @tod0.delete
+    render json: {todoId: @todo.id}
+  end
+
+
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:body)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
