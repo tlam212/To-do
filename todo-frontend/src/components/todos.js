7c14d5a4ed737b6eeec2082d19ee64a7f987a233
Toni Lamont <tlam212@gmail.com>
2019-12-17 12:37:49 -0500

    set up fetch and DOM rendering

--- todo-frontend/src/components/todos.js
+++ todo-frontend/src/components/todos.js
@@ -6,7 +6,27 @@ class Todos {
   }
   fetchTodos(){
     this.adapter.getTodos().then(todos => {
-      console.log(todos)
+      todos.forEach(todo => this.todos.push(todo))
     })
+    .then(data => {
+      this.renderToDom()
+    })
+  }
+
+  renderToDom(){
+    const todoContainer = document.getElementById('todo-container')
+    todoContainer.innerHTML = "new todo added"
+
+
+    // todoUl = document.createElement('ul')
+    // todoLi = document.createElement('li')
+    // todoSpan = document.createElement('span')
+    // todoSpan.classList.add('trashIcon')
+    // icon = document.createElement('i')
+    // icon.classList.add('fas fa-trash-alt')
+    // todoDiv.append(todoUl)
+    // todoUl.append(todoLi)
+    // to
+
   }
 }
